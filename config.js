0ec90a28c5eaffbd74db5bbb7e7a3044c8adf8d2
RexxzynXD mau S.Kom <rexxhayanasi@gmail.com>
2025-11-07 07:56:40 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+global.config = {
+  owner: '6210960622',      // ID Telegram kamu (tanpa @)
+  ownerName: 'RexxHayanasi', // Nama owner (opsional)
+  // Tombol di menu / info
+  menuBtn: '☰ MENU',
+  infoBtn: '☰ INFO',
+  pingBtn: '☰ PING',
+  faktaBtn: '☰ FAKTA',
+  // API key dan URL default
+  apiUrl: 'https://api.nekohime.xyz',
+
+  // Informasi bot
+  namebot: 'Elaina MultiDevice',
+  versi: '1.0.3',
+
+  // Opsi lainnya
+  prefix: '/',        // Prefix default command
+  autoreload: true,   // Auto reload plugin (jika ingin dibuat watcher)
+  autoSaveDB: true,   // Auto save database
+  databasePath: './database.json'
+}
